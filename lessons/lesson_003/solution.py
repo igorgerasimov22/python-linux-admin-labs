a2eb471ea9ef9e82966a49b1838ff9b33f69adcb
Игорь Герасимов <igorgerasimov@MacBook-Air-Igor.local>
2026-06-29 20:04:17 +0300

    add lessons 003

---
+++ lessons/lesson_003/solution.py
@@ -0,0 +1,11 @@
+def print_header():
+    print("=" * 30)
+
+def print_footer():
+    print("=" * 30)
+
+print_header()
+print("Информация о сервере")
+print_footer()
+print("Python Linux Admin")
+print_footer()
