aeb3c8518287a42d5effb196c583a4f44371f456
Игорь Герасимов <igorgerasimov@MacBook-Air-Igor.local>
2026-06-30 14:08:59 +0300

    update lessons 007

--- lessons/lesson_007/solution.py
+++ lessons/lesson_007/solution.py
@@ -1,3 +1,6 @@
+def print_header():
+    print("=" * 30)
+
 servers = []
 servers.append("web01")
 servers.append("web02")
@@ -5,4 +8,8 @@ servers.append("db01")
 servers.append("backup01")
 servers.append("proxy01")
 
-print(servers)
+print_header()
+print(f"Список серверов\n")
+for i in range(len(servers)):
+    print(f"{i+1}. {servers[i]}")
+print_header()
